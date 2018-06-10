a4c2347cbcebfaae0946a57c0d00686d56e988df
drcribeiro <cribeiro@MacBook-Pro.local>
2018-06-10 17:17:07 -0400

    Fully Functional with Restrictions
    Fully functional.
    Restrictions:
    1. connects to Twitter API but the data returned is empty array or undefined.
    2. Spotify API works only for song titles with one word.

--- README.md
+++ README.md
@@ -2,13 +2,16 @@
 1. What the project does
 1. Why the project is useful
 1. How users can get started with the project
-1. Where users can get help with your project
-1. Who maintains and contributes to the project
 
 
 **1. What the project does:**
-	
+	This Language Interpretation and Recognition Interface (LIRI) is a command line node application that takes in arguments and returns data from the following APIs: Twitter, Spotify, and Open Movie Database (OMDB). Commands can also be saved to a file and excuted with the application. User commands are logged to a file.
+
 **1. Why the project is useful:**
+	* Connects to different useful APIs (Twitter, Spotify, and Open Movie Database).
+	* Processes different commands from command line arguments as well as reading commands from a file and outputing all user commands to a seperate file.
+	* Uses NodeJS framework and multiple packages.
+
 **1. How users can get started with the project:** 
 	* install the following modules:
 		* npm i request
@@ -16,5 +19,6 @@
 		* npm i dotenv
 		* npm i node-spotify-api
 	* Use Request to grab data from the [OMDB API](http://www.omdbapi.com) 
-**1. Where users can get help with your project:**
-**1. Who maintains and contributes to the project:** 
+	* API keys need to be saved to a .env file.  Sign up as a developer with Spotify, Twitter, and OMDB to obtain your own API keys. These keys must be kept secret.
+		* The .env file will be used by the `dotenv` package to set environment variables to the global `process.env` object in node. 
+		* When commiting this code use gitignore on this file, so that it won't be pushed to github &mdash; keeping your API key information private.
--- liri.js
+++ liri.js
@@ -1,41 +1,43 @@
-// read and set any environment variables with the 
+// Read and set any environment variables with the 
 require("dotenv").config(); // dotenv package
-
 var keys = require("./keys.js"); // import keys.js and store it in a variable
-//console.log(keysImported);  // output: [object Object]
-//console.log(keys);
-//console.log(keys.spotify);
-//console.log(keys.twitter);
-//console.log("OMDB Key: ");
-//console.log(keys.omdb);
 
-// **************************************
-// should be able to access your keys information like so
-//var spotify = new Spotify(keys.spotify);
-//var client = new Twitter(keys.twitter);
+// Log commands to file log.txt
+const fs = require("fs"); // global constant to read and append to files
 
 
-// Handle user commands
- if(process.argv.length === 2){ // if user enters zero parameters
- 	zeroParameters();
- }
- else if(process.argv.length > 2){ 
+// Handle user commands through command line arguments
+if(process.argv.length === 2){ // if user enters zero parameters
+	appendToFile("No command entered by user" + "\n");
+	zeroParameters();
+}
+else if(process.argv.length > 2){ 
 	if(process.argv[2] === "my-tweets"){ // 1. cmd: 'node liri.js my-tweets'
+		appendToFile(process.argv[2] + "\n");
 		myTweets();  
 	}
 	else if(process.argv[2] === "spotify-this-song"){ // 2. cmd: 'node liri.js spotify-this-song "<song name here>"'
-		spotifyThisSong(); 
+		spotifyThisSong(process.argv.length, process.argv[2], process.argv[3]);
 	} 
 	else if(process.argv[2] === "movie-this"){ // 3. cmd: 'node liri.js movie-this "<movie name here>"'
- 		movieThis();
- 	}
- 	else if(process.argv[2] === "do-what-it-says"){ // 4. cmd: 'node liri.js do-what-it-says'
+		movieThis(process.argv.length, process.argv[2], process.argv[3]);
+	}
+	else if(process.argv[2] === "do-what-it-says"){ // 4. cmd: 'node liri.js do-what-it-says'
+		appendToFile(process.argv[2] + "\n");
 		doWhatItSays(); 
 	}
 }
 
 
-function zeroParameters(){ // if user enters zero parameters
+// Functions:
+function appendToFile(text){ 
+	fs.appendFile("log.txt", text, function(error) {
+		if(error) throw error;
+	});
+}
+
+
+function zeroParameters(){ // If user enters zero parameters
 	console.log("The command " + process.argv[2] + " is not a valid command."); //ask user to enter a correct cmd
 	console.log("Please enter one of the following valid commands: ");
 	console.log("my-tweets");
@@ -45,74 +47,124 @@ function zeroParameters(){ // if user enters zero parameters
 }
 
 
-function myTweets(){ // command 1
-	console.log("show last 20 tweets & when they were created");
+function myTweets(){ // Command 1
+	// Twitter API Reference: https://developer.twitter.com/en/docs/tweets/post-and-engage/api-reference/get-favorites-list
+	// Twitter Node Package Reference: https://www.npmjs.com/package/twitter
+	var Twitter = require('twitter');
+	var client = new Twitter(keys.twitter);
+	client.get('favorites/list', function(error, tweets, response) {
+		if(error) throw error;
+		console.log("Tweets: ");
+		console.log(tweets); // Tweet body
+		console.log("Created at: ");
+		console.log(response.created_at); // When tweets were created at
+		console.log();
+		console.log("Response: "); 
+		console.log(response.body); // Raw response object
+	});
 }
 
 
-function spotifyThisSong(){ //command 2
-	//console.log("show song info for " + process.argv[3]);
-	if(process.argv.length === 4){ // if correct number of arguments are provided
-			console.log("correct number of parameters");			
+//postTweet();
+function postTweet(){ // Extra function of Twitter API
+	var Twitter = require('twitter');
+	var client = new Twitter(keys.twitter);
+
+	client.post('statuses/update', {status: "This is very cool! I'm tweeting from my app."},  function(error, tweet, response) {
+	  if(error) throw error;
+	  console.log(tweet);  // Tweet body
+	  console.log(response);  // Raw response object
+	}); 
+}
+
+
+function spotifyThisSong(argvLength, cmd, songName){ // Command 2
+	if(argvLength === 4) { // If correct number of arguments are provided
+		appendToFile(cmd + " " + songName + "\n");
+		console.log("songName: " + songName);
+		spotifySong(songName); 
+	}
+	else { // No song provided by user
+		appendToFile(cmd + "\n");
+		spotifySong('Rise'); 	
+	}
+}
+
+
+//****** Doesnt work with non capital and with >1 word song name
+function spotifySong(songName){ // Subfunction of command 2
+	// Spotify Node Package Reference: https://www.npmjs.com/package/node-spotify-api
+	var Spotify = require('node-spotify-api');
+	var spotify = new Spotify(keys.spotify);
+	
+	spotify.search({type: 'track', query: songName}, function(error, data){
+		if(error) 
+			return console.log('Error occurred: ' + error);
+		
+		// Data returns an array of all albums matching the query
+		var i = 0;
+		while(data.tracks.items[i].album.name != songName) { // Iterate the array until find an item that matches the query
+			i++;
 		}
-	else // ask user to re-run the app and supply the correct number of arguments
-		console.log("Please specify the song as the 4th argument");
-}
+		console.log("Artists:"); // "artists" is an array containing objects for each artist
+		for(var index=0; index<data.tracks.items[i].album.artists.length; index++){
+			console.log(data.tracks.items[i].album.artists[index].name); // no names in json just an object
+		}
+		console.log("Name: " + data.tracks.items[i].album.name);
+		console.log("Spotify preview link: " + data.tracks.items[i].preview_url);
+		console.log("Album: " + data.tracks.items[i].album.name);
+	});
+}	
 
 
-function movieThis(){ // command 3
+function movieThis(argvLength, cmd, movieName){ // Command 3
 	var omdbKey = keys.omdb.omdb_key;
 	var movieTitleProvided = false;
-	if(process.argv.length === 4){ // if correct number of arguments are provided
+
+	if(argvLength === 4) { // If correct number of arguments are provided
+		appendToFile(cmd + " " + movieName + "\n");
 		movieTitleProvided = true;
-		omdbRequest(movieTitleProvided, omdbRequestInput(omdbKey, convertMovieTitle(process.argv[3])));  // movie title paramenter entered by user
+		omdbRequest(movieTitleProvided, omdbRequestInput(omdbKey, convertMovieTitle(movieName)));  // movie title paramenter entered by user
 	}
-	else { // if user doesn't specify a movie output the movie "Mr. Nobody"
+	else { // If user doesn't specify a movie output the movie "Mr. Nobody"
+		appendToFile(cmd + "\n");
 		omdbRequest(movieTitleProvided, omdbRequestInput(omdbKey, 'Mr.+Nobody'));  	
 	}
 }
 
 
-function countWords(str) { //subfunction of movieThis function
+function countWords(str) { // Subfunction of movieThis function
   return str.trim().split(/\s+/).length;
 }
 
 
-function splitString(str){ //subfunction of movieThis function
+function splitString(str){ // Subfunction of movieThis function
 	return str.trim().split(/\s+/);
 }
 
 
-function convertMovieTitle(movieTitleInput){ //subfunction of movieThis function
+function convertMovieTitle(movieTitleInput){ // Subfunction of movieThis function
 	var movieTitle = "";
-	console.log("number of words in movieTitleInput = " + countWords(movieTitleInput));
-	console.log("splitString = " + splitString(movieTitleInput));
 
 	for(var i=0; i<countWords(movieTitleInput); i++){
 		movieTitle += splitString(movieTitleInput)[i];
 		if(i != countWords(movieTitleInput)-1)
 			movieTitle += "+";
-		console.log("word[" + i + "]: " + "movieTitle = " + movieTitle);
 	} 
-	console.log("movieTitle = " + movieTitle);
 	return movieTitle;
 }
 
 
-function omdbRequestInput(omdbKey, movieTitleInput){ //subfunction of movieThis function
-	requestURL = 'http://www.omdbapi.com/?t=' + movieTitleInput + '&apikey=' + omdbKey;
-	console.log("requestURL = " + requestURL);
-	return requestURL;
-
+function omdbRequestInput(omdbKey, movieTitleInput){ // Subfunction of movieThis function
+	return requestURL = 'http://www.omdbapi.com/?t=' + movieTitleInput + '&apikey=' + omdbKey;	
 }
 
 
-function omdbRequest(movieTitleProvided, requestURL){ //subfunction of movieThis function
+function omdbRequest(movieTitleProvided, requestURL){ // Subfunction of movieThis function
 	var request = require('request');
 	request(requestURL, function (error, response, body) {
 		if (!error && response.statusCode === 200) {
-			// if user enters movie title param & if they don't
-			console.log("Title: " + JSON.parse(body).Title); // Parse the body of the site and recover just the imdbRating
+			console.log("Title: " + JSON.parse(body).Title); 
 			console.log("Year: " + JSON.parse(body).Year); 
 			console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
 			console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
@@ -121,11 +173,10 @@ function omdbRequest(movieTitleProvided, requestURL){ //subfunction of movieThis
 			console.log("Plot: " + JSON.parse(body).Plot);
 			console.log("Actors: " + JSON.parse(body).Actors); 
 
-			if(!movieTitleProvided) { //if user doesn't enter movie title param, append this:
-				console.log("If you haven't watched " + JSON.parse(body).Title 
-					+ "then you should: " + "<http://www.imdb.com/title/" 
+			if(!movieTitleProvided) { // If user doesn't enter movie title param, append this:
+				console.log("If you haven't watched '" + JSON.parse(body).Title 
+					+ "' then you should: " + "<http://www.imdb.com/title/" 
 					+ JSON.parse(body).imdbID + "/>");
-				// not sure if > is the readMe.md notation or should be in the output	
 				console.log("It's on Netflix!");
 			}
 		}
@@ -133,6 +184,42 @@ function omdbRequest(movieTitleProvided, requestURL){ //subfunction of movieThis
 }
 
 
-function doWhatItSays(){ //command 4
-	console.log("do-what-it-says");
+function doWhatItSays(){ // Command 4
+	//const fs = require("fs"); // Node FS Read File, golbally declared for reading and appending to different files
+	fs.readFile('random.txt', 'utf8', readData);
+}
+
+
+function readData(err, data) { // Subfunction of command 4
+	var text = data.trim().split(/\,+/); // Splits cmd and argument
+	// Returns ['spotify-this-song', '"Rise"']
+	
+	if(text[0] === "") { // If file is empty
+		console.log("File is empty, returned: " + text[0] + text[1]);
+		zeroParameters();
+	}
+	else if(text[0] === "my-tweets"){
+		myTweets();	
+	}
+	else if(text[0] === "spotify-this-song"){
+		spotifyThisSong(countParams(text[1]), text[0], removeQuotes(text[1]));
+	}
+	else if(text[0] === "movie-this") {
+		movieThis(countParams(text[1]), text[0], removeQuotes(text[1]));
+	}
+}
+
+
+function countParams(name){
+	var argvLength = 3; // argvLength is at least 3 if "spotify-this-song" is read from file
+	if(name != null) // If the 4th argument is not null, the file specifies the song name
+		argvLength++; // Add the 4th argument to the argvLength
+	return argvLength; 
+}
+
+
+function removeQuotes(name){
+	if(name != null) // If the 4th argument is not null, the file specifies the song name
+		name = name.slice(1,name.length-1); // Remove " " around the songName otherwise method spotifyThisSong cannot process the input and error occurs
+	return name; 
 }
